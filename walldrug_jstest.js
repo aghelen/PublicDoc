949fcef267bdcccc02db9c967fa677f2aa265c5f
aghelen <48769667+aghelen@users.noreply.github.com>
2019-03-20 17:28:14 -0500

    walldrug test javascript file

---
+++ walldrug_jstest.js
@@ -0,0 +1,8 @@
+
+var x = document.getElementById("btn_submit_1");
+var y = document.getElementById("btn_submit_lastpage");
+window.alert("hello world");
+window.alert("btn1" + btn_submit_1);
+window.alert("btn2" + btn_submit_lastpage);
+
+
